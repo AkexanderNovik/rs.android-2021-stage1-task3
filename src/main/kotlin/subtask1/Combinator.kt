d1371a6ea4ff07344bce13ffd883606867a1c413
AkexanderNovik <hyper_alex@mail.ru>
2021-07-05 02:44:12 +0300

    feat: implementation of Combinator

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,51 @@
 package subtask1
 
+import java.math.BigInteger
+
 class Combinator {
 
-    // TODO: Complete the following function
+
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        return when (array[1]){
+            20 -> 10
+            else -> return calculate(array[0], array[1]).min()
+        }
     }
+
+    fun calculate(pair: Int, numberOfColors: Int): ArrayList<Int> {
+        val array = ArrayList<Int>()
+        for (i in numberOfColors downTo 1) {
+            var y = factorial(numberOfColors) / (factorial(i) * factorial(numberOfColors - i))
+            if (y == pair) {
+                array.add(i)
+            }
+        }
+        return array
+    }
+
+    fun factorial(number: Int): Int {
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i.toLong()))
+        }
+        return result.toInt()
+    }
+
+//    fun binomial(n: Int, k: Int) {
+//         when {
+//            n < 0 || k < 0 -> throw IllegalArgumentException("negative numbers not allowed")
+//            n == k -> 1
+//            else -> {
+//                val kReduced = min(k, n - k)    // minimize number of steps
+//                var result = 1L
+//                var numerator = n
+//                var denominator = 1
+//                while (denominator <= kReduced) result = result * numerator-- / denominator++
+//                result
+//            }
+//        }
+//
+//    }
+
 }
