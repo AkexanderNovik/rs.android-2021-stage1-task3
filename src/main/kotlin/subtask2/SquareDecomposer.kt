b32347d5b95a169d0edc8959d16e731cc47eb00f
Aliaksandr Novik4 <aliaksandr_novik4@epam.com>
2021-07-05 15:55:28 +0300

    feat: implementation of SquareDecomposer

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -2,8 +2,55 @@ package subtask2
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
+    var result = arrayListOf<Int>()
+
+
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        if (number <5 ){
+            return null
+        } else {
+        decompose(number.toLong())
+        return result.toTypedArray()
+        }
+    }
+
+    fun decompose(n: Long): ArrayList<Int> {
+        for (i in n - 1 downTo 2) {
+            val square = n * n
+            result = recurseDecompose(i, square)!!
+            if (result != null) break
+        }
+
+        println(result)
+//        println(result.toIntArray())
+        return result
+    }
+
+
+    fun recurseDecompose(i: Long, rem: Long): ArrayList<Int>? {
+        var x = rem
+//        var result = mutableListOf<Int>()
+        if (i * i == x) {
+            result.add(i.toInt())
+            return result
+//            return i.toString() + ""
+        } else if (i * i < x) {
+            x = x - i * i
+            for (j in i - 1 downTo 1) {
+                val tmp = recurseDecompose(j, x)
+                return if (tmp != null) {
+
+//                    println("$tmp $i")
+//                    "$tmp $i"
+                    result.add(i.toInt())
+                    result
+                } else {
+                    continue
+                }
+            }
+        } else {
+            return null
+        }
+        return null
     }
 }
