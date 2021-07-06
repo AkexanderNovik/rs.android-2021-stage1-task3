8a33efe45617bf4dfb54c995974ecc9031098276
Aliaksandr Novik4 <aliaksandr_novik4@epam.com>
2021-07-06 19:13:17 +0300

    refact: TelephoneFinder refactoring

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,71 +4,30 @@ class TelephoneFinder {
 
     private val result = mutableSetOf<String>()
 
+    private val combinations = mapOf(
+        "0" to "8",
+        "1" to "24",
+        "2" to "135",
+        "3" to "26",
+        "4" to "157",
+        "5" to "2468",
+        "6" to "359",
+        "7" to "48",
+        "8" to "5790",
+        "9" to "68"
+    )
+
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
 
         return if (number.toInt() >= 0) {
-            for (i in number.indices) {
-                changeToNeighbor(number, i)
+            number.indices.forEach { i ->
+                combinations["${number[i]}"]?.forEach { j ->
+                    result += number.take(i) + j + number.drop(i + 1)
+                }
             }
             result.toTypedArray()
         } else {
             null
         }
-    }
-
-    private fun changeToNeighbor(s: String, index: Int) {
-        val strBf = StringBuffer(s)
-        when (strBf[index]) {
-            '1' -> {
-                alterStringBuffer(strBf, index, '2')
-                alterStringBuffer(strBf, index, '4')
-            }
-            '2' -> {
-                alterStringBuffer(strBf, index, '1')
-                alterStringBuffer(strBf, index, '3')
-                alterStringBuffer(strBf, index, '5')
-            }
-            '3' -> {
-                alterStringBuffer(strBf, index, '2')
-                alterStringBuffer(strBf, index, '6')
-            }
-            '4' -> {
-                alterStringBuffer(strBf, index, '1')
-                alterStringBuffer(strBf, index, '5')
-                alterStringBuffer(strBf, index, '7')
-            }
-            '5' -> {
-                alterStringBuffer(strBf, index, '2')
-                alterStringBuffer(strBf, index, '4')
-                alterStringBuffer(strBf, index, '6')
-                alterStringBuffer(strBf, index, '8')
-            }
-            '6' -> {
-                alterStringBuffer(strBf, index, '3')
-                alterStringBuffer(strBf, index, '5')
-                alterStringBuffer(strBf, index, '9')
-            }
-            '7' -> {
-                alterStringBuffer(strBf, index, '4')
-                alterStringBuffer(strBf, index, '8')
-                strBf.setCharAt(index, '4')
-            }
-            '8' -> {
-                alterStringBuffer(strBf, index, '5')
-                alterStringBuffer(strBf, index, '7')
-                alterStringBuffer(strBf, index, '9')
-                alterStringBuffer(strBf, index, '0')
-            }
-            '9' -> {
-                alterStringBuffer(strBf, index, '6')
-                alterStringBuffer(strBf, index, '8')
-            }
-            '0' -> alterStringBuffer(strBf, index, '8')
-        }
-    }
-
-    private fun alterStringBuffer(strBf: StringBuffer, index: Int, char: Char) {
-        strBf.setCharAt(index, char)
-        result.add(strBf.toString())
     }
 }
